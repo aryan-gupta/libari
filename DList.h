7993d3fe20679ed9eef164740b54d8c113346172
Aryan Gupta <kuantum.freak@gmail.com>
2017-05-25 20:36:43 -0400

    Defined DList

--- DList.h
+++ DList.h
@@ -16,3 +16,65 @@
  */
 
 #pragma once
+
+template <typename TYPE>
+class DList {
+public:
+	class iterator {
+	public:
+		iterator& operator++(); // preincrement
+		iterator& operator--();
+		
+		iterator& operator++(int); // postincrement
+		iterator& operator--(int);
+		
+		iterator& operator=(const iterator& it);
+		iterator& operator=(iterator&& it);
+		
+		iterator& operator-(const int scale);
+		iterator& operator+(const int scale);
+		
+		bool operator==(const iterator& it);
+		
+		TYPE& operator*();
+		TYPE& operator->();
+		
+	private:
+		struct Node* data;
+		
+	};
+	
+	DList();
+	DList(const DList& other);
+	
+	DList(const size_t size, const TYPE& = TYPE());
+	
+	template <typename ITER>
+	DList(const ITER& begin, const ITER& end);
+	
+	iterator begin() const;
+	iterator end() const;
+	
+	void clear();
+	
+	void push_back(const TYPE& val);
+	void push_front(const TYPE& val);
+	
+	void pop_back();
+	void pop_front();
+	
+	void remove(const TYPE& val);
+	
+	size_t size();
+	
+private:
+	struct Node {
+		Node* prev;
+		TYPE data;
+		Node* next;
+	};
+	
+	Node* mHead;
+	size_t mSize;
+	
+}
