28f6dcef327928dc0606b8fb3d5a0401fab24e64
Aryan Gupta <kuantum.freak@gmail.com>
2017-05-26 22:13:19 -0400

    Add More Tests

--- main.cpp
+++ main.cpp
@@ -24,13 +24,30 @@ using namespace std;
 #include "DList.h"
 
 int main(int argc, char* argv[]) {
-	vector<int> tmp = {0, 3, 1, 4, 2, 6, 7, 3, 7, 1, 9, 0, 6};
+	vector<int> tmp = {0, 3, 1, 4, 2, 6};
 	
 	DList<int> listTest(tmp.begin(), tmp.end());
 	
-	for(size_t i = 0; i < listTest.size(); ++i) {
+	listTest.push_back(5);
+	cout << listTest.size() << endl;
+	
+	listTest.push_back(7);
+	cout << listTest.size() << endl;
+	
+	listTest.push_front(2);
+	cout << listTest.size() << endl;
+	
+	
+	for(size_t i = 0; i < listTest.size(); ++i)
 		cout << listTest[i] << " ";
-	}
+	cout << endl;
+	
+	listTest.pop_back();
+	cout << listTest.size() << endl;
+	
+	for(size_t i = 0; i < listTest.size(); ++i)
+		cout << listTest[i] << " ";
+	cout << endl;
 	
 	return 0;
 }
