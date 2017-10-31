a1b18d14bf9d8ca9480997051753f59d0c86d43c
Aryan Gupta <kuantum.freak@gmail.com>
2017-10-31 07:48:48 -0400

    Add member types

--- unordered_mapp.hpp
+++ unordered_mapp.hpp
@@ -13,7 +13,26 @@ template <
 	typename TAlloc
 > class unordered_map {
 public:
-
+	using key_type             = TKey;
+	using mapped_type          = TType;
+	using value_type           = std::pair<const key_type, mapped_type>;
+	using size_type            = size_t;
+	using difference_type      = ptrdiff_t;
+	using hasher               = THash;
+	using key_equal            = TKeyEq;
+	using allocator_type       = TAlloc;
+	using reference            = value_type&;
+	using const_reference      = const value_type&;
+	using pointer              = typename std::allocator_traits<allocator_type>::pointer;
+	using const_pointer        = typename std::allocator_traits<allocator_type>::const_pointer;
+	/*using iterator             = ForwardIterator
+	using const_iterator       = Constant forward iterator
+	using local_iterator       = 
+	using const_local_iterator = 
+	using node_type            = 
+	using insert_return_type   = */
+	
+	
 private:
 
 };
