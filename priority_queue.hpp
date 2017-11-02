22437520b3eb86abb8c44621958e9bf6e93d654d
Aryan Gupta <kuantum.freak@gmail.com>
2017-11-01 22:07:05 -0400

    Use dependent typenames rather than template typenames

--- priority_queue.hpp
+++ priority_queue.hpp
@@ -43,34 +43,34 @@ public:
 	using container_type  = TCont;
 	using value_type      = TType;
 	using value_compare   = TPred;
-	using size_type       = typename TCont::size_type;
-	using reference       = typename TCont::reference;
-	using const_reference = typename TCont::const_reference;
-
-	priority_queue(TPred cmp);
-	priority_queue(TCont cont);
-	priority_queue(TCont cont, TPred cmp);
-	priority_queue(std::initializer_list<TType> lst);
-	priority_queue(std::initializer_list<TType> lst, TPred cmp);
-	priority_queue(const priority_queue<TType, TCont, TPred>& other);
-	priority_queue(priority_queue<TType, TCont, TPred>&& other);
+	using size_type       = typename container_type::size_type;
+	using reference       = typename container_type::reference;
+	using const_reference = typename container_type::const_reference;
+	
+	priority_queue(value_compare cmp);
+	priority_queue(container_type cont);
+	priority_queue(container_type cont, value_compare cmp);
+	priority_queue(std::initializer_list<value_type> lst);
+	priority_queue(std::initializer_list<value_type> lst, value_compare cmp);
+	priority_queue(const priority_queue& other);
+	priority_queue(priority_queue&& other);
 	
 	template <typename TIter>
 	priority_queue(TIter begin, TIter end);
 	
 	template <typename TIter>
-	priority_queue(TIter begin, TIter end, TPred cmp);
+	priority_queue(TIter begin, TIter end, value_compare cmp);
 	
-	priority_queue<TType, TCont, TPred>& operator=(const priority_queue<TType, TCont, TPred>& other);
-	priority_queue<TType, TCont, TPred>& operator=(priority_queue<TType, TCont, TPred>&& other);
+	priority_queue& operator=(const priority_queue& other);
+	priority_queue& operator=(priority_queue&& other);
 	
 	
-	void push(TType element);
+	void push(value_type element);
 	const_reference top() const;
 	void pop();
 	size_type size() const;
 	bool empty() const;
-	void swap(priority_queue<TType, TCont, TPred>& other);
+	void swap(priority_queue& other);
 	void priority_queueify();
 	
 //#if __cplusplus >= 201703L // becaise I'm using g++ 6.4 to compile
@@ -83,7 +83,7 @@ public:
 #else
 	
 	template <typename TRet, typename... TArgs>
-	auto call_member(TRet TCont::* member, TArgs... args);
+	auto call_member(TRet container_type::* member, TArgs... args);
 	
 	template <typename TLamb, typename... TArgs>
 	auto call_member(TLamb lambda, TArgs... args);
@@ -91,98 +91,98 @@ public:
 #endif
 	
 private:
-	void priority_queueifyup(typename TCont::iterator idx);
-	void priority_queueifydown(typename TCont::iterator idx);
+	using iterator = typename container_type::iterator;
 	
-	typename TCont::iterator getLeftChIt(typename TCont::iterator idx);
-	typename TCont::iterator getRightChIt(typename TCont::iterator idx);
-	typename TCont::iterator getParentIt(typename TCont::iterator idx);
+	void heapifyup(iterator idx);
+	void heapifydown(iterator idx);
 	
-	container_type mpriority_queue;
+	iterator getLeftChIt(iterator idx);
+	iterator getRightChIt(iterator idx);
+	iterator getParentIt(iterator idx);
+	
+	container_type mHeap;
 	value_compare mComp;
 	
 };
 
 
 template <typename TType, typename TCont, typename TPred>
-priority_queue<TType, TCont, TPred>::priority_queue(TPred cmp)
-: mpriority_queue{}, mComp{cmp} { /* No Code */ }
+priority_queue<TType, TCont, TPred>::priority_queue(value_compare cmp)
+: mHeap{}, mComp{cmp} { /* No Code */ }
 
 
 template <typename TType, typename TCont, typename TPred>
-priority_queue<TType, TCont, TPred>::priority_queue(TCont cont)
-: mpriority_queue{cont}, mComp{} {
+priority_queue<TType, TCont, TPred>::priority_queue(container_type cont)
+: mHeap{cont}, mComp{} {
 	priority_queueify();
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-priority_queue<TType, TCont, TPred>::priority_queue(TCont cont, TPred cmp)
-: mpriority_queue{cont}, mComp{cmp} {
+priority_queue<TType, TCont, TPred>::priority_queue(container_type cont, value_compare cmp)
+: mHeap{cont}, mComp{cmp} {
 	priority_queueify();
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-priority_queue<TType, TCont, TPred>::priority_queue(std::initializer_list<TType> lst)
-: mpriority_queue{lst.begin(), lst.end()}, mComp{} {
+priority_queue<TType, TCont, TPred>::priority_queue(std::initializer_list<value_type> lst)
+: mHeap{lst.begin(), lst.end()}, mComp{} {
 	priority_queueify();
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-priority_queue<TType, TCont, TPred>::priority_queue(std::initializer_list<TType> lst, TPred cmp)
-: mpriority_queue{lst.begin(), lst.end()}, mComp{cmp} {
+priority_queue<TType, TCont, TPred>::priority_queue(std::initializer_list<value_type> lst, value_compare cmp)
+: mHeap{lst.begin(), lst.end()}, mComp{cmp} {
 	priority_queueify();
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-priority_queue<TType, TCont, TPred>::priority_queue(const priority_queue<TType, TCont, TPred>& other) 
-: mpriority_queue{other.mpriority_queue}, mComp{other.mComp} { /* No Code */ }
+priority_queue<TType, TCont, TPred>::priority_queue(const priority_queue& other) 
+: mHeap{other.mHeap}, mComp{other.mComp} { /* No Code */ }
 
 
 template <typename TType, typename TCont, typename TPred>
-priority_queue<TType, TCont, TPred>::priority_queue(priority_queue<TType, TCont, TPred>&& other)
-: mpriority_queue{other.mpriority_queue}, mComp{other.mComp} { /* No Code */ }
+priority_queue<TType, TCont, TPred>::priority_queue(priority_queue&& other)
+: mHeap{other.mHeap}, mComp{other.mComp} { /* No Code */ }
 
 
 template <typename TType, typename TCont, typename TPred>
 template <typename TIter>
 priority_queue<TType, TCont, TPred>::priority_queue(TIter begin, TIter end)
-: mpriority_queue{begin, end} {
+: mHeap{begin, end} {
 	priority_queueify();
 }
 
 
 template <typename TType, typename TCont, typename TPred>
 template <typename TIter>
-priority_queue<TType, TCont, TPred>::priority_queue(TIter begin, TIter end, TPred cmp)
-: mpriority_queue{begin, end}, mComp{cmp} {
+priority_queue<TType, TCont, TPred>::priority_queue(TIter begin, TIter end, value_compare cmp)
+: mHeap{begin, end}, mComp{cmp} {
 	priority_queueify();
 }
 
 
-template <typename TType, typename TCont, typename TPred> // template parameter
-priority_queue<TType, TCont, TPred>& // return type
-priority_queue<TType, TCont, TPred>::operator=(const priority_queue<TType, TCont, TPred>& other) { // member function
+template <typename TType, typename TCont, typename TPred> 
+auto priority_queue<TType, TCont, TPred>::operator=(const priority_queue& other) -> priority_queue& {
 	if (&other == this)
 		return *this;
 	
-	mpriority_queue = other.mpriority_queue;
+	mHeap = other.mHeap;
 	mComp = other.mComp;
 	
 	return *this;
 }
 
 
-template <typename TType, typename TCont, typename TPred> // template parameter
-priority_queue<TType, TCont, TPred>& // return type
-priority_queue<TType, TCont, TPred>::operator=(priority_queue<TType, TCont, TPred>&& other) { // member function
+template <typename TType, typename TCont, typename TPred>
+auto priority_queue<TType, TCont, TPred>::operator=(priority_queue&& other) -> priority_queue& {
 	if (&other == this)
 		return *this;
 	
-	mpriority_queue = other.mpriority_queue;
+	mHeap = other.mHeap;
 	mComp = other.mComp;
 	
 	return *this;
@@ -192,51 +192,51 @@ priority_queue<TType, TCont, TPred>::operator=(priority_queue<TType, TCont, TPre
 template <typename TType, typename TCont, typename TPred>
 void priority_queue<TType, TCont, TPred>::priority_queueify() {
 	for (
-		auto b = mpriority_queue.begin() - 1, e = std::next(mpriority_queue.begin(), mpriority_queue.size() / 2 - 1);
+		auto b = mHeap.begin() - 1, e = std::next(mHeap.begin(), mHeap.size() / 2 - 1);
 		b != e; 
 		--e) { // use reverse iterators
-		priority_queueifydown(e);
+		heapifydown(e);
 	}
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-void priority_queue<TType, TCont, TPred>::push(TType element) {
-	mpriority_queue.push_back(element);
-	priority_queueifyup(mpriority_queue.end() - 1);
+void priority_queue<TType, TCont, TPred>::push(value_type element) {
+	mHeap.push_back(element);
+	heapifyup(mHeap.end() - 1);
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-typename priority_queue<TType, TCont, TPred>::const_reference priority_queue<TType, TCont, TPred>::top() const {
-	return mpriority_queue.front(); // make sure that front is available
+auto priority_queue<TType, TCont, TPred>::top() const -> const_reference {
+	return mHeap.front(); // make sure that front is available
 	// for most containers
 }
 
 
 template <typename TType, typename TCont, typename TPred>
 void priority_queue<TType, TCont, TPred>::pop() {
-	mpriority_queue.front() = mpriority_queue.back();
-	mpriority_queue.pop_back(); // change this to erase an iterator
-	priority_queueifydown(mpriority_queue.begin());
+	mHeap.front() = mHeap.back();
+	mHeap.pop_back(); // change this to erase an iterator
+	heapifydown(mHeap.begin());
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-typename priority_queue<TType, TCont, TPred>::size_type priority_queue<TType, TCont, TPred>::size() const {
-	return mpriority_queue.size();
+auto priority_queue<TType, TCont, TPred>::size() const -> size_type {
+	return mHeap.size();
 }
 
 
 template <typename TType, typename TCont, typename TPred>
 bool priority_queue<TType, TCont, TPred>::empty() const {
-	return mpriority_queue.empty();
+	return mHeap.empty();
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-void priority_queue<TType, TCont, TPred>::swap(priority_queue<TType, TCont, TPred>& other) {
-	mpriority_queue.swap(other.mpriority_queue);
+void priority_queue<TType, TCont, TPred>::swap(priority_queue& other) {
+	mHeap.swap(other.mHeap);
 }
 
 //#if __cplusplus >= 201703L
@@ -245,46 +245,46 @@ void priority_queue<TType, TCont, TPred>::swap(priority_queue<TType, TCont, TPre
 template <typename TType, typename TCont, typename TPred>
 template <typename TFunc, typename... TArgs>
 auto priority_queue<TType, TCont, TPred>::call_member(TFunc func, TArgs... args) {
-	return std::invoke(func, mpriority_queue, std::forward<TArgs>(args)...);
+	return std::invoke(func, mHeap, std::forward<TArgs>(args)...);
 }
 
 #else
 
 template <typename TType, typename TCont, typename TPred>
 template <typename TRet, typename... TArgs>
-auto priority_queue<TType, TCont, TPred>::call_member(TRet TCont::* member, TArgs... args) {
-	return (mpriority_queue.* member) (args...);
+auto priority_queue<TType, TCont, TPred>::call_member(TRet container_type::* member, TArgs... args) {
+	return (mHeap.* member) (args...);
 }
 
 
 template <typename TType, typename TCont, typename TPred>
 template <typename TLamb, typename... TArgs>
 auto priority_queue<TType, TCont, TPred>::call_member(TLamb lambda, TArgs... args) {
-	return lambda(mpriority_queue, args...);
+	return lambda(mHeap, args...);
 }
 
 #endif
 
 template <typename TType, typename TCont, typename TPred>
-void priority_queue<TType, TCont, TPred>::priority_queueifyup(typename TCont::iterator idx) {
+void priority_queue<TType, TCont, TPred>::heapifyup(iterator idx) {
 	// I need to check for copies, some of these things could be turned
 	// into refeerences
-	auto begin = mpriority_queue.begin(); // use reverse iterators
+	auto begin = mHeap.begin(); // use reverse iterators
 	auto pt = getParentIt(idx); 
 	
 	if (std::distance(begin, pt) >= 0 and mComp(*idx, *pt)) {
 		std::iter_swap(idx, pt);
-		priority_queueifyup(pt);
+		heapifyup(pt);
 	}
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-void priority_queue<TType, TCont, TPred>::priority_queueifydown(typename TCont::iterator idx) {
+void priority_queue<TType, TCont, TPred>::heapifydown(iterator idx) {
 	auto lc  = getLeftChIt(idx); 
 	auto rc  = getRightChIt(idx);
 	auto max = idx;
-	auto end = mpriority_queue.end();
+	auto end = mHeap.end();
 	
 	if (std::distance(lc, end) > 0 and mComp(*lc, *max))
 		max = lc;
@@ -294,26 +294,26 @@ void priority_queue<TType, TCont, TPred>::priority_queueifydown(typename TCont::
 	
 	if (idx != max) {
 		std::iter_swap(idx, max);
-		priority_queueifydown(max);
+		heapifydown(max);
 	}
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-typename TCont::iterator priority_queue<TType, TCont, TPred>::getLeftChIt(typename TCont::iterator idx) {
-	return std::next(mpriority_queue.begin(), std::distance(mpriority_queue.begin(), idx) * 2 + 1);
+auto priority_queue<TType, TCont, TPred>::getLeftChIt(iterator idx) -> iterator {
+	return std::next(mHeap.begin(), std::distance(mHeap.begin(), idx) * 2 + 1);
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-typename TCont::iterator priority_queue<TType, TCont, TPred>::getRightChIt(typename TCont::iterator idx) {
-	return std::next(mpriority_queue.begin(), std::distance(mpriority_queue.begin(), idx) * 2 + 2);
+auto priority_queue<TType, TCont, TPred>::getRightChIt(iterator idx) -> iterator {
+	return std::next(mHeap.begin(), std::distance(mHeap.begin(), idx) * 2 + 2);
 }
 
 
 template <typename TType, typename TCont, typename TPred>
-typename TCont::iterator priority_queue<TType, TCont, TPred>::getParentIt(typename TCont::iterator idx) {
-	return std::next(mpriority_queue.begin(), (std::distance(mpriority_queue.begin(), idx) - 1) / 2);
+auto priority_queue<TType, TCont, TPred>::getParentIt(iterator idx) ->iterator {
+	return std::next(mHeap.begin(), (std::distance(mHeap.begin(), idx) - 1) / 2);
 }
 
 } // end namespace ari
