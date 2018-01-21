89d608029a62a93af8ffeb4f836e47961dfeff81
Aryan Gupta <kuantum.freak@gmail.com>
2018-01-21 00:13:17 -0500

    Improve to conform to standard

--- include/barrier.hpp
+++ include/barrier.hpp
@@ -9,42 +9,64 @@
 
 namespace ari {
 
+
 class barrier {
 	size_t mThreads;
 	size_t mWaiting;
-	bool mCycle; // Boost makes this a size_t, however a bool will suffice and
-	// potentially uses less memory.
+	bool mCycle = false; // Boost makes this a size_t, however a bool will suffice and
+	// potentially uses less memory. 
+	
+	bool mComplete = false;
+	thread_local bool mIsPart = false;
 	
 	std::mutex mMtx;
 	std::condition_variable mCondVar;
 	
 public:
-	explicit barrier(ptrdiff_t num_threads)
-	    : mThreads{ num_threads }, mWaiting{ num_threads }, mCycle{ false } {};
+	explicit barrier(ptrdiff_t num_threads) : mThreads{ num_threads }, mWaiting{ num_threads } {};
 	barrier(const barrier&) = delete;
 	
 	barrier& operator=(const barrier&) = delete;
-	~barrier() = default;
+	~barrier() {
+		std::scoped_lock lk{ mMtx };
+		mCycle = !mCycle;
+		mCondVar.notify_all();
+	};
 	
 	void arrive_and_wait() {
 		std::unique_lock lk{ mMtx };
 		
-		--mWaiting;
+		if (!mComplete)	{ // if we are not past the completion phase,
+			//then mark this thread as part of the barrier
+			mIsPart = true; 
+		} 
 		
+		if (mComplete and !mIsPart) { // Standard does not state what happens if
+			// if a non-participationg member calls this function.
+			return; 
+		} 
+		
+		// executes if complete and is a member
+		--mWaiting;
+	
 		if (mWaiting == 0) { // we are the last one waiting
+			if (!mComplete) mComplete = true;
+			
 			mWaiting = mThreads;
 			mCycle = !mCycle;
 			lk.unlock();
 			mCondVar.notify_all();
-			return;
+		} else {
+			bool cycle_local = mCycle;
+			mCondVar.wait(lk, [this]{ return mCycle == cycle_local; });	
 		}
-		
-		bool cycle_local = mCycle;
-		mCondVar.wait(lk, [&]{ return mCycle == cycle_local; });
-		
 	}
 	
-	void arrive_and_drop();
+	void arrive_and_drop() {
+		std::scoped_lock lk{ mMtx };
+		if (mIsPart) mIsPart = false;
+		--mThreads;
+	}
 	
 };
 
